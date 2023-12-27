c4e2bda70f8897dc9d969b7151a92245d12bbe6f
Farooq Umer <pvufarooq@gmail.com>
2023-12-27 13:05:02 +0530

    Auth APIs and unit tests
    docker files updated
    
    unit tests
    
    new fields added

--- user/models.py
+++ user/models.py
@@ -1,5 +1,20 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
+from .enums import UserRole
 
 class User(AbstractUser):
-    pass
+
+    is_student = models.BooleanField(default=False)
+    is_faculty = models.BooleanField(default=False)
+
+    @property
+    def role(self) -> UserRole:
+        """Return the user's role."""
+
+        if self.is_superuser:
+            return UserRole.ADMIN
+        elif self.is_student:
+            return UserRole.STUDENT
+        elif self.is_faculty:
+            return UserRole.FACULTY
---
+++ user/tests/factory/user.py
@@ -0,0 +1,25 @@
+import factory
+from user.models import User
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    first_name = factory.Faker("first_name")
+    last_name = factory.Faker("last_name")
+    username = factory.LazyAttribute(
+        lambda obj: f"{obj.first_name.lower()}.{obj.last_name.lower()}"
+    )
+    email = factory.LazyAttribute(
+        lambda obj: f"{obj.first_name.lower()}.{obj.last_name.lower()}@example.com"
+    )
+    password = factory.PostGenerationMethodCall("set_password", "defaultpassword")
+    is_active = True
+    is_staff = False
+    is_superuser = False
+
+
+class SuperUserFactory(UserFactory):
+    is_superuser = True
+    is_staff = True
